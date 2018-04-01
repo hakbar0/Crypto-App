5787989a7da5a171ea71eb0e52a785e5da830b89
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-01 13:13:39 +0100

    Making the app modular.

--- react-native-redux-crypto-tracker/App.js
+++ react-native-redux-crypto-tracker/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { Header } from './src/components/index.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
+      <View>
+        <Header />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
