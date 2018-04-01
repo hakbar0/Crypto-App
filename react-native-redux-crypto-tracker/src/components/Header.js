85bbdd2d604869eda1eca3e2e197560effd0c2a3
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-01 14:44:43 +0100

    Made links between redux and react.

--- react-native-redux-crypto-tracker/src/components/Header.js
+++ react-native-redux-crypto-tracker/src/components/Header.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 const Header = () => {
   return (
     <View style={headerContainer}>
-      <Text style={header}>Crypto App</Text>
+      <Text style={header}>Crypto App!</Text>
     </View>
   )
 }
